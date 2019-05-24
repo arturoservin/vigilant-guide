1f37481751bf86604fdbcbd66c7279bfeb8d3f16
arturoservin <50938310+arturoservin@users.noreply.github.com>
2019-05-23 21:59:39 -0500

    Update imc.js

--- imc.js
+++ imc.js
@@ -9,3 +9,11 @@ altura = prompt("cual es tu altura? (en centimetros)");
 resultado_imc = (peso / (altura**2))*10000;
 
 document.write("tu IMC es " + resultado_imc);
+//Validamos que se cumpla el rango
+if (resultado_imc > 18.5||resultado_imc < 24.9 ) {
+  document.write("su imc es normal para un adulto promedio")
+}
+
+else {
+
+}
